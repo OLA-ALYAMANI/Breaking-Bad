d5da4b12d83d6d87b5add394538f1f09014f5251
OLA13 <o-m-yamani@hotmail.com>
2020-07-27 20:12:41 +0300

    Part 3

--- src/components/Characters/CharacterGrid.js
+++ src/components/Characters/CharacterGrid.js
@@ -2,12 +2,12 @@ import React from 'react'
 import CharacterItem from './CharacterItem'
 
 
-const CharacterGrid = ({ itmes, isLoading }) => {
+const CharacterGrid = ({ items, isLoading }) => {
     return isLoading ? 
     (<h1>Loading...</h1>)
     :
     (<section className='cards'>
-        {itmes.map(item => (
+        {items.map(item => (
             <CharacterItem key={item.chart_id} item={item}></CharacterItem>
         ))}
     </section>
---
+++ src/components/ui/Search.js
@@ -0,0 +1,13 @@
+import React, { useState }from 'react'
+
+const Search = () => {
+  return (
+      <section>
+          <form>
+              <input type='text' className='form-control' placeholder="Search characters"/>
+          </form>
+      </section>
+  )
+}
+
+export default Search
