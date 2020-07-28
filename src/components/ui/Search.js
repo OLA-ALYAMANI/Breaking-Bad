e0dc2166dae9af5552be2b7cfea9e2b62887d55a
OLA13 <o-m-yamani@hotmail.com>
2020-07-28 18:47:44 +0300

    Breabing app reactJS

--- src/components/ui/Search.js
+++ src/components/ui/Search.js
@@ -1,10 +1,21 @@
 import React, { useState }from 'react'
 
-const Search = () => {
+const Search = ({ getQuery }) => {
+    const [text, setText] = useState('')
+
+    const onChange = (q) => {
+        setText(q)
+        getQuery(q)
+    }
   return (
-      <section>
+      <section className="search">
           <form>
-              <input type='text' className='form-control' placeholder="Search characters"/>
+              <input type='text'
+              className='form-control'
+              placeholder="Search characters"
+              value={text}
+              onChange={(e) => onChange(e.target.value)}
+              autoFocus/>
           </form>
       </section>
   )
